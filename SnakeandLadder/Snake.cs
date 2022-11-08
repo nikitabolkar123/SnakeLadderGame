20dd722af78fd6a99bc785afc08d23db2c00ff31
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-08 02:30:21 -0800

    Added UC

--- SnakeandLadder/Snake.cs
+++ SnakeandLadder/Snake.cs
@@ -4,13 +4,43 @@ using System.Text;
 
 namespace SnakeandLadder
 {
+
     internal class Snake
     {
+        public const int Ahead_Move = 1;
+        public const int Behind_Move = 2;
+        public const int No_Move = 3;
         public void player()
         {
+            int position = 0;
             Random random = new Random();
-            int Die = random.Next(1, 6);
-            Console.WriteLine($"Roll Die is : {Die}");
+            int MovingCheck = random.Next(1, 6);
+            int StepCheck = random.Next(1, 3);
+            Console.WriteLine($"Roll Die is : {MovingCheck}");
+            Console.WriteLine($"Moving Check is : {StepCheck}");
+
+            switch (StepCheck)
+            {
+                case Ahead_Move:
+
+                    position = position + MovingCheck;
+                    Console.WriteLine("Ahead Move");
+
+                    break;
+                case Behind_Move:
+
+                    position = position - MovingCheck;
+                    Console.WriteLine("Behind Move");
+
+                    break;
+                case No_Move:
+
+                    position = position + MovingCheck;
+                    Console.WriteLine("No Move");
+
+                    break;
+            }
+            Console.WriteLine("Step Count is :" + position);
         }
     }
 }

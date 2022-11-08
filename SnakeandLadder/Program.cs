e6c853eef90177026e8470a146d8a89fc3e29cb5
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-08 02:10:11 -0800

    Added UC

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake and Ladder Problem !!");
+            Snake snake = new Snake();
+            snake.player();
+        }
+    }
+}
---
+++ SnakeandLadder/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeandLadder
+{
+    internal class Snake
+    {
+        public void player()
+        {
+            int Num_Player = 1;
+            int Start_Position = 0;
+
+            Console.WriteLine($" Number Of Player is :{Num_Player}");
+            Console.WriteLine($" Start Position is :{Start_Position}");
+        }
+    }
+}
